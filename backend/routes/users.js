0f6a6297705185a3ce1784ad07b86d82bd505fd1
tuneontan <115734700+tuneontan@users.noreply.github.com>
2022-11-24 13:48:01 +0100

    token and hash

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+require('../models/connection');
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
 
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
@@ -19,11 +22,14 @@ router.post('/signup', (req, res) => {
   // Check if the user has not already been registered
   User.findOne({ username: req.body.username }).then(data => {
     if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
       const newUser = new User({
         firstname: req.body.firstname,
         username: req.body.username,
-        password: req.body.password,
-      });
+        password: hash,
+        token: uid2(32),
+       });
 
       newUser.save().then(() => {
         res.json({ result: true });
@@ -41,13 +47,23 @@ router.post('/signin', (req, res) => {
     return;
   }
 
-  User.findOne({ username: req.body.username, password: req.body.password }).then(data => {
-    if (data) {
-      res.json({ result: true });
-    } else {
-      res.json({ result: false, error: 'User not found' });
-    }
-  });
-});
+//   User.findOne({ username: req.body.username, password: req.body.password }).then(data => {
+//     if (data) {
+//       res.json({ result: true });
+//     } else {
+//       res.json({ result: false, error: 'User not found' });
+//     }
+//   });
+// });
+
+
+User.findOne({ username: req.body.username }).then(data => {
+  if (bcrypt.compareSync(req.body.password, data.password)) {
+    res.json({ result: true });
+  } else {
+    res.json({ result: false });
+  }
+ });
+})
 
 module.exports = router;
