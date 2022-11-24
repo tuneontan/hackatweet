1d79630f744acaa7718af40f5a6d80e2ceb2a5fb
OussamaDabachil <oussamadabachilpro@gmail.com>
2022-11-24 14:07:58 +0100

    push signup button

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -3,6 +3,15 @@ import {useState} from 'react'
 import { set } from 'mongoose';
 function Home() {
 
+  const[firstnameI,setFirstnameI]=useState()
+
+  const[usernameI,setUsernameI]=useState()
+  const[passwordI,setPasswordI]=useState()
+
+  const[usernameC,setUsernameC]=useState()
+  const[passwordC,setPasswordC]=useState()
+
+
   const [appearSignUp,setAppearSignUp] = useState(false)
   const [appearSignIn,setAppearSignIn] = useState(false)
   const [appearBlack,setAppearBlack] = useState(false)
@@ -64,6 +73,15 @@ function Home() {
     setAppearSignIn(false)
   }
 
+
+  const signUp =()=>{
+    
+
+  }
+
+  const signIn =()=>{
+    
+  }
   return (
     <div>
 
@@ -73,18 +91,28 @@ function Home() {
         <h3>Create your Hackatweet account</h3>
         <span onClick={()=>HideSUBlack()}>x</span>
         <img src='twitter_logo_white.png' alt='logo twitter'></img>
-        <input type='text' placeholder='firstname'></input>
-        <input type='text' placeholder='username'></input>
-        <input type='text' placeholder='password'></input>
-        <button>Sign In</button>
+        <input type='text'
+        onChange={(e) => setFirstnameI(e.target.value)} value={firstnameI}
+         placeholder='firstname'></input>
+        <input 
+         onChange={(e) => setUsernameI(e.target.value)} value={usernameI}
+        type='text' placeholder='username'></input>
+        <input 
+        onChange={(e) => setPasswordI(e.target.value)} value={passwordI}
+        type='text' placeholder='password'></input>
+        <button >Sign Up</button>
       </div>
 
       <div className={styles.signUpPopUp} style={signInStyle}>
         <h3>Sign In</h3>
         <span onClick={()=>HideSIBlack()}>x</span>
         <img src='twitter_logo_white.png' alt='logo twitter'></img>
-        <input type='text' placeholder='username'></input>
-        <input type='text' placeholder='password'></input>
+        <input 
+        onChange={(e) => setUsernameC(e.target.value)} value={usernameC}
+        type='text' placeholder='username'></input>
+        <input
+        onChange={(e) => setPasswordC(e.target.value)} value={passwordC}
+        type='text' placeholder='password'></input>
         <button>Sign In</button>
       </div>
       <main className={styles.main}>
